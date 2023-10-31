52b099f6646658a8bb3e191efd9b982e6a061b3d
mamta-jaiswal20 <mj20042001@gmail.com>
2023-10-31 14:39:46 +0530

    Varibles in java

---
+++ Variables/Variables.java
@@ -0,0 +1,23 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        // Declare variables of different types: int, double, boolean and String.
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println(num1 * num2);
+
+        // String Example
+        String name = new String("Mamta");
+        System.out.println(name.length());
+
+        // Assigning result to another varible and printing it
+        int n1 = 4;
+        int n2 = 7;
+        int result = n1 + n2;
+        System.out.println("Result of sum is: " + result);
+
+    }
+
+}
